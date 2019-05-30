a173e0066d0202a6e53283875eb2c99564bd9a4e
m0r016 <m0r016@m0r016-mba.local>
2019-05-31 03:09:44 +0900

    2019.05.31 Topに戻る

--- common/common.js
+++ common/common.js
@@ -17,3 +17,49 @@ $(function () {
         // 通常の動きをキャンセルさせる = 上で指定した動きを適用させるため
     });
 });
+
+$(function () {
+    // $topBtnにback-to-topボタンのjQueryオブジェクトを代入
+    var $topBtn = $('.back-to-top');
+
+    // 最初はボタンを隠しておく
+    $('.back-to-top').hide();
+
+    // ボタンの表示条件
+    $(window).scroll(function () {
+        // スクロール量が500以上になったら
+        if ($(this).scrollTop() > 250) {
+            // ボタンを表示させる
+            $topBtn.fadeIn();
+        } else {
+            // 500未満の場合は表示させない
+            $topBtn.fadeOut();
+        }
+    });
+
+    // ボタンクリックでページ内要素に移動
+    // 特定の移動先が無い場合はページトップへ移動
+    // aタグのhref属性の値が#始まりの場合のみ実行する
+    $('a[href^="#"]').click(function () {
+        // スクロールの速さ
+        var speed = 500;
+        // ボタンのhref属性の値を取得
+        var href = $(this).attr("href");
+        // ボタンのhref属性が#のみまたは存在しなければhtmlを返す
+        // 値があればそれを取得する
+        var $target = $(href == "#" || href == "" ? 'html' : href);
+        // ボタンの位置の高さを取得する
+        // $('html').offset().topの値は0になる
+        var position = $target.offset().top;
+        // body,htmlは両方指定する
+        // positionで指定したスクロール位置に移動する
+        // animateで動作が完了するまでの速さ(speed)と動作パターン(swing)を指定する
+        $('body,html').animate({
+            scrollTop: position
+        }, speed, 'swing');
+
+        // イベントのデフォルト効果を打ち消す=関数内で指定した動作しか行わない
+        return false;
+        // event.preventDefault();
+    });
+});
